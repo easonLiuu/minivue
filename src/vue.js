4c712cda311a2f345cb92c6d69067fa73023c20b
liujiarui <ljr3289595084@163.com>
2022-10-16 21:38:24 +0800

    add proxy

--- src/vue.js
+++ src/vue.js
@@ -7,11 +7,33 @@ class Vue {
         this.$methods = options.methods
         //监视data中的数据
         new Observer(this.$data)
+        //把data中所有数据代理到vm上
+        this.proxy(this.$data)
+        //把methods中所有数据代理到vm上
+        this.proxy(this.$methods)
         //如果指定了el参数，对el进行解析
         if (this.$el) {
             //compile负责解析模版的内容
             //需要：模版和数据 this为vue实例 把整个实例传过去
             new Compile(this.$el, this)            
         }
+    }
+
+    proxy(data) {
+        Object.keys(data).forEach(key => {
+            Object.defineProperty(this, key, {
+                enumerable: true,
+                configurable: true,
+                get() {
+                    return data[key]
+                },
+                set(newValue) {
+                    if(data[key] == newValue) {
+                        return
+                    }
+                    data[key] = newValue
+                }
+            })
+        })
     }
 }
